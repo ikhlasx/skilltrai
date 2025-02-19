27d4c528b556918df4c86c343dfd442d7e9e1cab
ikhlasx <ikhlas110@gmail.com>
2025-02-19 21:38:13 +0530

    Fix trainers issues

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'index.html')
+
+
 def courses(request):
     return render(request, 'courses.html')
+
+
+def trainers(request):
+    return render(request, 'trainers.html')
--- skilltrai/urls.py
+++ skilltrai/urls.py
@@ -4,4 +4,5 @@ from frontend import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('courses/', views.courses, name='courses'),
+    path('trainers/', views.trainers, name='trainers'),
 ]
